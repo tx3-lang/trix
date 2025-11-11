e660c6204549aa8c16510354b01b92261c131d0d
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-11-11 17:48:37 -0300

    refactor: replace Expect enum with ExpectUtxo struct for improved clarity

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -30,7 +30,7 @@ struct Test {
     file: PathBuf,
     wallets: Vec<Wallet>,
     transactions: Vec<Transaction>,
-    expect: Vec<Expect>,
+    expect: Vec<ExpectUtxo>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -48,53 +48,17 @@ struct Transaction {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-enum Expect {
-    Balance(ExpectBalance),
-    // TODO: improve expect adding more options
+struct ExpectUtxo {
+    from: String,
+    datum_equals: Option<serde_json::Value>,
+    min_amount: Option<Vec<ExpectMinAmount>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ExpectBalance {
-    wallet: String,
-    amount: ExpectAmount,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum ExpectAmount {
-    Absolute(u64),
-    Aprox(ExpectAmountAprox),
-}
-
-impl ExpectAmount {
-    pub fn matches(&self, value: u64) -> bool {
-        match self {
-            ExpectAmount::Absolute(x) => x.eq(&value),
-            ExpectAmount::Aprox(x) => {
-                let lower = x.target.saturating_sub(x.threshold);
-                let upper = x.target + x.threshold;
-                value >= lower && value <= upper
-            }
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ExpectAmountAprox {
-    target: u64,
-    threshold: u64,
-}
-
-impl Display for ExpectAmount {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ExpectAmount::Absolute(value) => write!(f, "{value}"),
-            ExpectAmount::Aprox(value) => {
-                write!(f, "target: ~{} (+/- {})", value.target, value.threshold)
-            }
-        }
-    }
+struct ExpectMinAmount {
+    policy: Option<String>,
+    name: Option<String>,
+    amount: u64,
 }
 
 fn ensure_test_home(test: &Test, hashable: &[u8]) -> Result<PathBuf> {
@@ -233,26 +197,22 @@ pub fn run(args: Args, _config: &Config, profile: &ProfileConfig) -> Result<()> 
     }
 
     for expect in test.expect.iter() {
-        match expect {
-            Expect::Balance(expect) => {
-                let balance = crate::spawn::cshell::wallet_balance(&test_home, &expect.wallet)?;
+        // let balance = crate::spawn::cshell::wallet_balance(&test_home, &expect.from)?;
+        todo!();
+        // let r#match = expect.amount.matches(balance.coin);
 
-                let r#match = expect.amount.matches(balance.coin);
+        // if !r#match {
+        //     failed = true;
 
-                if !r#match {
-                    failed = true;
+        //     eprintln!(
+        //         "Test Failed: `{}` Balance did not match the expected result.",
+        //         expect.from
+        //     );
+        //     eprintln!("Expected: {}", expect.amount);
+        //     eprintln!("Received: {}", balance.coin);
 
-                    eprintln!(
-                        "Test Failed: `{}` Balance did not match the expected result.",
-                        expect.wallet
-                    );
-                    eprintln!("Expected: {}", expect.amount);
-                    eprintln!("Received: {}", balance.coin);
-
-                    eprintln!("Hint: Check the tx3 file or the test file.");
-                }
-            }
-        }
+        //     eprintln!("Hint: Check the tx3 file or the test file.");
+        // }
     }
 
     if !failed {
